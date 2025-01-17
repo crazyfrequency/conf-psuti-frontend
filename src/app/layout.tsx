9e3efdae270d43074078829f200a7527b14f5407
crazyfrequency <di.efimoff2015@gmail.com>
2025-01-17 20:14:51 +0400

    Убрано свойство серверного компонента для корневого layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,17 +1,15 @@
-'use server'
-
 import I18nProvider from "@/components/layout/I18nProvider";
 import { Toaster } from "@/components/ui/sonner";
 import { ThemeProvider } from "next-themes";
 import "./globals.css";
 
-export default async function RootLayout({
+export default function RootLayout({
   children
 }: Readonly<{
   children: React.ReactNode
 }>) {
   return (
-    <html>
+    <html suppressHydrationWarning>
       <body className="antialiased">
         <ThemeProvider
           attribute="class"
@@ -19,8 +17,8 @@ export default async function RootLayout({
           enableSystem
         >
           <I18nProvider>
+            <Toaster duration={5000} pauseWhenPageIsHidden closeButton richColors />
             {children}
-            <Toaster />
           </I18nProvider>
         </ThemeProvider>
       </body>
